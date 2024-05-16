f19e443b35253e8a192d1ae0ef3751e6ca092c35
Franisco maikel <francisco.gomes664@aluno.ce.gov.br>
2024-05-16 15:16:37 -0300

    add user created validations

--- src/controllers/userControllers.ts
+++ src/controllers/userControllers.ts
@@ -1,6 +1,40 @@
 import { Request, NextFunction, Response } from "express";
+import { string, z } from "zod";
 
 export const userControllers = {
+  async create(req: Request, res: Response, next: NextFunction) {
+    try {
+      const userSchema = z.object({
+        name: z
+          .string({
+            required_error: "name is required!",
+            invalid_type_error: "name must be a string!",
+          })
+          .min(3, "name must have at least 3 characters!")
+          .max(255, "max name lenght exceeded!"),
+
+        email: string({
+          required_error: "name is required!",
+          invalid_type_error: "name must be a string!",
+        })
+          .email("email poorly formated")
+          .max(255, "max email length exceeded!"),
+        password: string({
+          required_error: "name is required!",
+          invalid_type_error: "name must be a string!",
+        })
+          .min(7, "password must have at least 7 characters")
+          .max(255, "max password lenght exceeded!"),
+      });
+
+      const { name, email, password } = userSchema.parse(req.body);
+
+      return res.status(201).json({ message: "User created!" });
+    } catch (error) {
+      return next(error);
+    }
+  },
+
   async read(req: Request, res: Response, next: NextFunction) {
     try {
       return res.status(200).json({ message: "User read!" });
@@ -8,4 +42,4 @@ export const userControllers = {
       return next(error);
     }
   },
- };
+};
